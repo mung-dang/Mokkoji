58dd211d38a121813248f1cee715959798da62fb
윤예은 <yeayen123@gmail.com>
2022-08-22 16:59:18 +0900

    #2. 로그인, 회원가입 액티비티 생성

--- app/src/main/java/com/example/mokkoji/LoginActivity.kt
+++ app/src/main/java/com/example/mokkoji/LoginActivity.kt
@@ -3,15 +3,15 @@ package com.example.mokkoji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.example.mokkoji.databinding.ActivityMainBinding
+import com.example.mokkoji.databinding.ActivityLoginBinding
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
     }
--- app/src/main/java/com/example/mokkoji/SignupActivity.kt
+++ app/src/main/java/com/example/mokkoji/SignupActivity.kt
@@ -3,15 +3,15 @@ package com.example.mokkoji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.example.mokkoji.databinding.ActivityMainBinding
+import com.example.mokkoji.databinding.ActivitySignupBinding
 
-class MainActivity : BaseActivity() {
+class SignupActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivitySignupBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signup)
         setupEvents()
         setValues()
     }
