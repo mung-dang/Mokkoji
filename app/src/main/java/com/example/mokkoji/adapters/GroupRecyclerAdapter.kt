1b8383dc55b8ba4c0c3857b180d9d466ad7edb67
윤예은 <yeayen123@gmail.com>
2022-08-23 15:08:31 +0900

    #2. 리싸이클러뷰 연결중

---
+++ app/src/main/java/com/example/mokkoji/adapters/GroupRecyclerAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.mokkoji.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mokkoji.R
+import com.example.mokkoji.datas.GroupData
+
+class GroupRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<GroupData>
+) : RecyclerView.Adapter<GroupRecyclerAdapter.MyViewHolder>(){
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        fun bind(item : GroupData){
+            val title = itemView.findViewById<TextView>(R.id.titleTxt)
+            val groupExp = itemView.findViewById<TextView>(R.id.groupExpTxt)
+
+            title.text = item.title
+            groupExp.text = item.groupExp
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val row = LayoutInflater.from(mContext).inflate(R.layout.group_list_item, parent, false)
+        return MyViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(mList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return  mList.size
+    }
+}
